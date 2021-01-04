8ebc0f11fbf2b256037b182270457267cc80a09f
周家合 <18631736558@163.com>
2021-01-04 19:09:59 +0800

    name

--- 1.js
+++ 1.js
@@ -1,2 +1,3 @@
 const str = "Git is a distributed version control system";
 const str1 ="Git is a distributed version control system.Git is free software distributed under the GPL."
+const name = "周家合"
